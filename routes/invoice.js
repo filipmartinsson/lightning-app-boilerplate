a1200ab298f843d441c340a72dd93078133fbbbc
Filip Martinsson <martinsson.filip@gmail.com>
2019-05-30 15:15:23 +0200

    removed env

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const BTCPAY_PRIV_KEY = "";
+const BTCPAY_MERCHANT_KEY = "";
 
 // Initialize the client
 const btcpay = require('btcpay')
-const keypair = btcpay.crypto.load_keypair(new Buffer.from(process.env.BTCPAY_PRIV_KEY, 'hex'));
-const client = new btcpay.BTCPayClient('https://lightning.filipmartinsson.com', keypair, {merchant: process.env.BTCPAY_MERCHANT_KEY})
+const keypair = btcpay.crypto.load_keypair(new Buffer.from(BTCPAY_PRIV_KEY, 'hex'));
+const client = new btcpay.BTCPayClient('https://lightning.filipmartinsson.com', keypair, {merchant: BTCPAY_MERCHANT_KEY})
 
 
 /* get & verify invoice. */
